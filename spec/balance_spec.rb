fb538d46052fbed075fc7a43ecd76b6e455ced46
marie taylor <marietaylortailor@hotmail.com>
2023-03-22 14:09:54 +0000

    minus balance for balance only

--- spec/balance_spec.rb
+++ spec/balance_spec.rb
@@ -13,5 +13,13 @@ RSpec.describe AccountHolder do
       .to output("date       ||credit  ||debit   ||balance \n01-03-2023|| || || 100.0 \n").to_stdout
       end
     end
+
+    it 'displays balance only' do
+      account_holder = AccountHolder.new('01-03-2023', nil, nil, -100.0)
+      expect do
+        account_holder.print_statement
+      .to output("date       ||credit  ||debit   ||balance \n01-03-2023|| || || -100.0 \n").to_stdout
+      end
+    end
   end
 end
